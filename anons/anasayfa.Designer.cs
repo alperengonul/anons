72b7e475f28d2a9e074843f23663b82200908f9b
alperengonul <82762490+alperengonul@users.noreply.github.com>
2022-05-30 15:48:41 +0300

    yer

--- anons/anasayfa.Designer.cs
+++ anons/anasayfa.Designer.cs
@@ -362,7 +362,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(408, 19);
+            this.label3.Location = new System.Drawing.Point(327, 9);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(28, 13);
             this.label3.TabIndex = 5;
